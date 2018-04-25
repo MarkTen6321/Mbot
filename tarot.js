a70150962bdf40df7bb81dcb4a8f368c9563d560
MarkTen6321 <markten6321@gmail.com>
2018-04-25 12:42:39 +0530

    Add files via upload

---
+++ tarot.js
@@ -0,0 +1,157 @@
+module.exports = {
+    a: {
+        about0: "Beginnning, Innocence, Spontaniety, Free Soul",
+        about1: "Naivety, Foolishness, Reclessness, Risk-Taking",
+        link0: "https://gfx.tarot.com/images/site/decks/universal-waite/full_size/0.jpg",
+        link1: ""
+    },
+
+    b: {
+        about0: "Power, Skill, Concentration, Action, Resourcefulness",
+        about1: "Manipulation, Poor Planing, Latent Talents",
+        link0: "https://gfx.tarot.com/images/site/decks/universal-waite/full_size/1.jpg",
+        link1: ""
+    },
+
+    c: {
+        about0: "Intuition, Higher Powers, Mystery, Subconscious Mind",
+        about1: "Hidden Agendas, Need to listen Inner Voice",
+        link0: "https://gfx.tarot.com/images/site/decks/universal-waite/full_size/2.jpg",
+        link1: ""
+    },
+
+    d: {
+        about0: "Fertility, Feminity, Beauty, Nature, Abundance",
+        about1: "Creative Block, Depndence on others",
+        link0: "https://gfx.tarot.com/images/site/decks/universal-waite/full_size/3.jpg",
+        link1: ""
+    },
+
+    e: {
+        about0: "Authority, Father figure, Structure, Solid Foundation",
+        about1: "Dominations, Excessive Control, Rigidity, Inflexibility",
+        link0: "https://gfx.tarot.com/images/site/decks/universal-waite/full_size/4.jpg",
+        link1: ""
+    },
+
+    f: {
+        about0: "Relegion, Group, Identification, Conformity, Tradition, Beliefs",
+        about1: "Restirction, Chalenging the status quo",
+        link0: "https://gfx.tarot.com/images/site/decks/universal-waite/full_size/5.jpg",
+        link1: ""
+    },
+
+    g: {
+        about0: "Love, Union, Relationships, Values, Alignment, Choices",
+        about1: "Disharmony, Imbalance, Misalignment of Values",
+        link0: "https://gfx.tarot.com/images/site/decks/universal-waite/full_size/6.jpg",
+        link1: ""
+    },
+
+    h: {
+        about0: "Control, Will Power, Victory, Assertion, Determination",
+        about1: "Lack of Control and Direction, Agression",
+        link0: "https://gfx.tarot.com/images/site/decks/universal-waite/full_size/7.jpg",
+        link1: ""
+    },
+
+    i: {
+        about0: "Strength, Courage, Patience, Control, Compassion",
+        about1: "Weekness, Self Doubt, Lack of Discipline",
+        link0: "https://gfx.tarot.com/images/site/decks/universal-waite/full_size/8.jpg",
+        link1: ""
+    },
+
+    j: {
+        about0: "Soul Searching, Introspection, Being Alone, Inner Guidance",
+        about1: "Isolation, Loneliness, Withdrawl",
+        link0: "https://gfx.tarot.com/images/site/decks/universal-waite/full_size/9.jpg",
+        link1: ""
+    },
+
+    k: {
+        about0: "Good Luck, Karma, Life Cycles, Destiny, a Turning Point",
+        about1: "Bad Luck, Negative Externa Forces, Out of Control",
+        link0: "https://gfx.tarot.com/images/site/decks/universal-waite/full_size/10.jpg",
+        link1: ""
+    },
+
+    l: {
+        about0: "Justice, Fairness, Truth, Cause and Effect, Law",
+        about1: "Unfairness, Lack of Accountability, Dishonesty",
+        link0: "https://gfx.tarot.com/images/site/decks/universal-waite/full_size/11.jpg",
+        link1: ""
+    },
+
+    m: {
+        about0: "Suspension, Restrition, Letting Go, Sacrifice",
+        about1: "Martydom, Indecision, Delay",
+        link0: "https://gfx.tarot.com/images/site/decks/universal-waite/full_size/12.jpg",
+        link1: ""
+        ,
+    },
+
+    o: {
+        about0: "Endings, Beginnings, Change, Transformation, transition",
+        about1: "Resistance to change, Unable to move on",
+        link0: "https://gfx.tarot.com/images/site/decks/universal-waite/full_size/13.jpg",
+        link1: ""
+    },
+
+    p: {
+        about0: "Balance, Moderation, Patience, Purpose, Meaning",
+        about1: "Imbalance, Excess, Lack of Long Term Vision",
+        link0: "https://gfx.tarot.com/images/site/decks/universal-waite/full_size/14.jpg",
+        link1: ""
+    },
+
+    q: {
+        about0: "Bondage, Addiction, Sexuaity, Materialism",
+        about1: "Detachment, Breaking Free, Power Reclaimed",
+        link0: "https://gfx.tarot.com/images/site/decks/universal-waite/full_size/15.jpg",
+        link1: ""
+    },
+
+    r: {
+        about0: "Disaster, Upheaval, Sudden Change, Revelation",
+        about1: "Avoidance of Disaster, Fear of Change",
+        link0: "https://gfx.tarot.com/images/site/decks/universal-waite/full_size/16.jpg",
+        link1: ""
+    },
+
+    s: {
+        about0: "Hope, Spirtuality, Renewal, Inspiration, Serenity",
+        about1: "Lack of Faith, Despair, Discouragemnt",
+        link0: "https://gfx.tarot.com/images/site/decks/universal-waite/full_size/17.jpg",
+        link1: ""
+    },
+
+    t: {
+        about0: "Illusion, Fear, Anxiety, Insecurity, Subconscious",
+        about1: "Release of Fear, Unhappiness, Cofusion",
+        link0: "https://gfx.tarot.com/images/site/decks/universal-waite/full_size/18.jpg",
+        link1: ""
+    },
+
+    u: {
+        about0: "Fun, Warmth, Success, Postivity, Vitality",
+        about1: "Temporary Depression, Lack of Sucess",
+        link0: "https://gfx.tarot.com/images/site/decks/universal-waite/full_size/19.jpg",
+        link1: ""
+    },
+
+    v: {
+        about0: "Judgement, ReBirth, Inner Calling, Absolution",
+        about1: "Self Doubt, Refusal of Self Examination",
+        link0: "https://gfx.tarot.com/images/site/decks/universal-waite/full_size/20.jpg",
+        link1: ""
+    },
+
+    w: {
+        about0: "Completion, Integration, Accomplishment, Travel",
+        about1: "Lack of Completion, Lack of Closure",
+        link0: "https://gfx.tarot.com/images/site/decks/universal-waite/full_size/21.jpg",
+        link1: ""
+    }
+
+}
