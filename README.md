64d21070d97d9e4e6ac19cfbaa85c81fb5adf3a7
Sagnik Pradhan <markten6321@gmail.com>
2019-08-09 22:16:54 +0530

    Fixed typos, Added README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# MBot rewrite in Python
+
+This bot is completely written in Python and it aims to help Developers
--- firestore.py
+++ firestore.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate("./mrpool-firebase-adminsdk-svtnn-1b17d72bcc.json")
+cred = credentials.Certificate("./Firebase-admin-creds.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
