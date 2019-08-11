217581c1ef34938a9466bc95263e5ba48f81f79e
Dries1234 <driesolbrechts@gmail.com>
2019-08-11 19:40:27 +0200

    nothing important

--- cogs/tag.py
+++ cogs/tag.py
@@ -1,90 +1,99 @@
 import re
 import asyncio
-import discord 
-from firestore import db 
+import discord
+from firestore import db
 from difflib import SequenceMatcher
 import json
 from discord.ext import commands
 
 
 class tag(commands.Cog):
-    def __init__(self,bot):
+    def __init__(self, bot):
         self.bot = bot
-    #Create a group for our sub commands 
-    @commands.group(invoke_without_command = True)
-    async def tag(self,ctx,title: str,*content):
+
+    # Create a group for our sub commands
+    @commands.group(invoke_without_command=True)
+    async def tag(self, ctx, title: str, *content):
         content = ' '.join(content)
         if ctx.invoked_subcommand is None:
-            
-        
-            tag_doc = db.document('discord_tags/{0}/tags/{1}'.format(ctx.guild.id,title)).get()
-            tag_field = tag_doc.to_dict() #This is needed to fetch fields
+
+            tag_doc = db.document(
+                'discord_tags/{0}/tags/{1}'.format(ctx.guild.id, title)).get()
+            tag_field = tag_doc.to_dict()  # This is needed to fetch fields
             if tag_doc.exists:
-                await ctx.send(tag_field['description'].replace('<br>','\n'))
+                await ctx.send(tag_field['description'].replace('<br>', '\n'))
             else:
                 color = discord.Color.from_rgb(237, 0, 0)
                 description = 'That tag does not exist'
-                embed = discord.Embed(color= color, description= description)
+                embed = discord.Embed(color=color, description=description)
                 await ctx.send(embed=embed)
-            
+
     @tag.command()
-    async def add(self,ctx,title:str,*content):
+    async def add(self, ctx, title: str, *content):
         content = ' '.join(content)
-        
-        tag_title = db.document('discord_tags/{0}/tags/{1}'.format(ctx.guild.id,title)).get()
+
+        tag_title = db.document(
+            'discord_tags/{0}/tags/{1}'.format(ctx.guild.id, title)).get()
         if tag_title.exists:
             color = discord.Color.from_rgb(237, 0, 0)
             description = 'That tag already exists'
-            embed = discord.Embed(color= color, description= description)
+            embed = discord.Embed(color=color, description=description)
             await ctx.send(embed=embed)
         else:
-            #Create a document with fields
+            # Create a document with fields
             data = {
                 u'title': title,
                 u'description': content,
                 u'author_id': ctx.author.id,
                 u'author_name': ctx.author.name
             }
-            await handleTimeout(data,ctx)
+            await handleTimeout(data, ctx)
+
     @tag.command()
-    async def edit(self,ctx,title:str,*content):
+    async def edit(self, ctx, title: str, *content):
         content = ' '.join(content)
-        tag_title = db.document('discord_tags/{0}/tags/{1}'.format(ctx.guild.id,title)).get()
+        tag_title = db.document(
+            'discord_tags/{0}/tags/{1}'.format(ctx.guild.id, title)).get()
         if tag_title.exists:
-            tag_title.reference.update({'description': content}) #update changes a specified field in a document
+            # update changes a specified field in a document
+            tag_title.reference.update({'description': content})
             color = discord.Color.from_rgb(0, 106, 212)
             embedTitle = 'Edited tag'
             description = title
-            embed = discord.Embed(color= color, description= description, title = embedTitle)
+            embed = discord.Embed(
+                color=color, description=description, title=embedTitle)
             await ctx.send(embed=embed)
         else:
             await ctx.send('A tag with the name "{0}" does not exist, create a tag using `m!addtag [tag name] [value]'.format(title))
 
     @tag.command()
-    async def delete(self,ctx,title:str):
-        
-        tag_title = db.document('discord_tags/{0}/tags/{1}'.format(ctx.guild.id,title)).get()
-        
+    async def delete(self, ctx, title: str):
+
+        tag_title = db.document(
+            'discord_tags/{0}/tags/{1}'.format(ctx.guild.id, title)).get()
+
         if tag_title.exists:
-            tag_title.reference.delete() #You have to make it a reference to delete it
+            tag_title.reference.delete()  # You have to make it a reference to delete it
             color = discord.Color.from_rgb(227, 88, 2)
             embedTitle = 'Removed tag'
             description = title
-            embed = discord.Embed(color= color, description= description, title = embedTitle)
+            embed = discord.Embed(
+                color=color, description=description, title=embedTitle)
             await ctx.send(embed=embed)
         else:
             await ctx.send('A tag with the name "{0}" does not exist'.format(title))
 
 
-
-async def handleTimeout(tag,ctx):
-    db.collection(u'discord_tags').document(str(ctx.guild.id)).collection('tags').document(tag['title']).set(tag)
+async def handleTimeout(tag, ctx):
+    db.collection(u'discord_tags').document(str(ctx.guild.id)).collection(
+        'tags').document(tag['title']).set(tag)
     color = discord.Color.from_rgb(6, 201, 6)
     embedTitle = 'Added new tag'
     description = tag['title']
-    embed = discord.Embed(color= color, description= description, title = embedTitle)
+    embed = discord.Embed(
+        color=color, description=description, title=embedTitle)
     await ctx.send(embed=embed)
-         
+
+
 def setup(bot):
     bot.add_cog(tag(bot))
-
--- index.py
+++ index.py
@@ -1,16 +1,15 @@
+import traceback
+import sys
+from os.path import isfile, join
+from os import listdir
+import time
+from spam import handleDiscordSpam
 import discord
 from discord.ext import commands
 import json
 
 config = json.load(open('./config.json', 'r'))
-from spam import handleDiscordSpam
 
-import time
-
-from os import listdir
-from os.path import isfile, join
-
-import sys, traceback
 
 description = 'Meet Mbot the Fancy Bot!'
 
@@ -18,7 +17,7 @@ description = 'Meet Mbot the Fancy Bot!'
 cogs_dir = "cogs"
 
 
-bot = commands.Bot(command_prefix='m!', description = description)
+bot = commands.Bot(command_prefix='m!', description=description)
 
 if __name__ == '__main__':
     for extension in [f.replace('.py', '') for f in listdir(cogs_dir) if isfile(join(cogs_dir, f))]:
@@ -27,16 +26,19 @@ if __name__ == '__main__':
         except (discord.ClientException, ModuleNotFoundError):
             print(f'Failed to load extension {extension}.')
             traceback.print_exc()
-    
+
 
 @bot.event
 async def on_ready():
-  print('Logged in as: {0.name} \nID: {0.id} \nTimeStamp: {1}'.format(bot.user, time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())))
+    print('Logged in as: {0.name} \nID: {0.id} \nTimeStamp: {1}'.format(
+        bot.user, time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())))
+
+
 @bot.event
 async def on_message(message):
     if (not message.author == bot.user) and message.guild and config['guild_id'] == str(message.guild.id):
         await handleDiscordSpam(message)
     await bot.process_commands(message)
-  
+
 
 bot.run(config['token'])
