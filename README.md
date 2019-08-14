c79e9fab5936abb220aed09d1bdf285dc976653e
Sagnik Pradhan <markten6321@gmail.com>
2019-08-14 21:35:15 +0530

    Adding support for Heroku

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ This bot is completely written in Python and it aims to help Developers
 1. Clone the repository.
 2. Edit the `config.json` File
 3. Rename `example.env` to `.env` and put in your Bot Token
-4. Get credentials json file from Firebase Admin
+4. Get credentials json file from Firebase Admin or paste the JSON in the `.env` file
 5. Run `pip install -r requirements.txt`
 6. Run `py -3 index.py`
--- firestore.py
+++ firestore.py
@@ -2,7 +2,14 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate("./Firebase-admin-creds.json")
+from os import path, environ
+from json import loads
+
+# Either .json file or env json string
+if path.exists("./Firebase-admin-creds.json"): cred = "./Firebase-admin-creds.json"
+else: cred = loads(environ.get('firebase_cred'))
+
+cred = credentials.Certificate(cred)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
